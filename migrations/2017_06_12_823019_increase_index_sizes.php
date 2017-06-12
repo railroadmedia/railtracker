c5a9b4717d985cbb5ddfadac0b1399a81c338c59
calebfavor <calebfavor@gmail.com>
2017-06-12 12:07:29 -0700

    Fixing migration class name.

--- migrations/2017_06_12_823019_increase_index_sizes.php
+++ migrations/2017_06_12_823019_increase_index_sizes.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateRequestsTable extends Migration
+class IncreaseIndexSizes extends Migration
 {
     /**
      * Run the migrations.
