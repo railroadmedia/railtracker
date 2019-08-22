1b725b49b8e48e28b2e78b8ae0abfc05ac7a73a3
Jonathan <jonathan@drumeo.com>
2019-08-22 08:34:08 -0700

    Delete obsolete - ExceptionTracker

--- src/Trackers/ExceptionTracker.php
+++ src/Trackers/ExceptionTracker.php
@@ -8,9 +8,6 @@ use Illuminate\Cookie\CookieJar;
 use Illuminate\Database\DatabaseManager;
 use Illuminate\Routing\Router;
 use Railroad\Doctrine\Serializers\BasicEntitySerializer;
-use Railroad\Railtracker\Entities\Exception as ExceptionEntity;
-use Railroad\Railtracker\Entities\Request;
-use Railroad\Railtracker\Entities\RequestException;
 use Railroad\Railtracker\Managers\RailtrackerEntityManager;
 use Railroad\Railtracker\Services\BatchService;
 use Railroad\Railtracker\ValueObjects\ExceptionVO;
@@ -18,25 +15,14 @@ use Railroad\Railtracker\ValueObjects\RequestVO;
 
 class ExceptionTracker extends TrackerBase
 {
-    /**
-     * @var RequestTracker
-     */
-    private $requestTracker;
-
-    /*
-     * Argument 5 passed to Railroad\Railtracker\Trackers\TrackerBase::__construct() must be an instance of
-     * Railroad\Railtracker\Services\BatchService, instance of Illuminate\Cookie\CookieJar given, called in
-     * /app/railtracker/src/Trackers/ExceptionTracker.php on line 49
-     */
     public function __construct(
         DatabaseManager $databaseManager,
         Router $router,
         CookieJar $cookieJar,
-        Repository $cache = null,
         BatchService $batchService,
         BasicEntitySerializer $basicEntitySerializer,
         RailtrackerEntityManager $entityManager,
-        RequestTracker $requestTracker
+        Repository $cache = null
     ){
         parent::__construct(
             $databaseManager,
@@ -47,16 +33,14 @@ class ExceptionTracker extends TrackerBase
             $basicEntitySerializer,
             $entityManager
         );
-        $this->requestTracker = $requestTracker;
         $this->batchService = $batchService;
     }
 
     /**
      * @param Exception $exception
-     * @param $uuid
      * @return void
      */
-    public function trackException(Exception $exception, $uuid)
+    public function trackException(Exception $exception)
     {
         if(!empty(RequestVO::$UUID)){
             try {
@@ -71,29 +55,5 @@ class ExceptionTracker extends TrackerBase
                 error_log($exception);
             }
         }
-    }
-
-    /**
-     * @param $data
-     * @return ExceptionEntity|RequestException
-     * @throws Exception
-     */
-    public function hydrate($data)
-    {
-        $exceptionEntity = new ExceptionEntity();
-
-        $exceptionEntity->setCode($data['code'] ?? null);
-        $exceptionEntity->setLine($data['line'] ?? null);
-        $exceptionEntity->setExceptionClass($data['exceptionClass'] ?? null);
-        $exceptionEntity->setFile($data['file'] ?? null);
-        $exceptionEntity->setMessage($data['message'] ?? null);
-        $exceptionEntity->setTrace($data['trace'] ?? null);
-
-        $requestException = new RequestException();
-
-        $requestException->setException($exceptionEntity);
-        $requestException->setCreatedAtTimestampMs($data['createdAtTimestampMs']);
-
-        return $requestException;
     }
 }
