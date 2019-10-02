railroadmedia/railtracker

commit 120aa21497a3d44f0622544a3368da30025d78f6

Fixing ip address database group by error.

src/Repositories/RequestRepository.php
@@ -337,9 +337,10 @@ class RequestRepository extends TrackerRepositoryBase
 
         $matchingRequests = $this->databaseManager->connection($dbConnectionName)
             ->table($table)
+            ->select(['ip_address'])
             ->whereIn('ip_address', $ipAddresses)
             ->orderBy('requested_on', 'desc')
-            ->groupBy('ip_address')
+            ->groupBy(['ip_address', 'requested_on'])
             ->get();
 
         return $matchingRequests;

commit 25eb94a9500f1b466d6fe797b7f35fbfefbf4208

Fix ip-data failure introduced in previous commit (120aa21)

src/Console/Commands/ProcessTrackings.php
@@ -421,7 +421,7 @@ class ProcessTrackings extends \Illuminate\Console\Command
                 $requestVO->ipLongitude = $dbRowWithIpData->ip_longitude ?? null;
                 $requestVO->ipCountryCode = $dbRowWithIpData->ip_country_code ?? null;
                 $requestVO->ipCountryName = $dbRowWithIpData->ip_country_name ?? null;
-                $requestVO->ipRegion = $dbRowWithIpData->ip_region_name ?? null;
+                $requestVO->ipRegion = $dbRowWithIpData->ip_region ?? null;
                 $requestVO->ipCity = $dbRowWithIpData->ip_city ?? null;
                 $requestVO->ipPostalZipCode = $dbRowWithIpData->ip_postal_zip_code ?? null;
                 $requestVO->ipTimezone = $dbRowWithIpData->ip_timezone ?? null;
@@ -481,11 +481,6 @@ class ProcessTrackings extends \Illuminate\Console\Command
         $ipAddress = $requestVO->ipAddress;
         $matchingRequestsForIpAddress = $matchingRequests->where('ip_address', $ipAddress);
 
-        if($matchingRequestsForIpAddress->count() > 1){
-            error_log('There should only be one.');
-        }
-
         return $matchingRequestsForIpAddress;
     }
-
 }

src/Repositories/RequestRepository.php
@@ -337,10 +337,33 @@ class RequestRepository extends TrackerRepositoryBase
 
         $matchingRequests = $this->databaseManager->connection($dbConnectionName)
             ->table($table)
-            ->select(['ip_address'])
+            ->select([
+                'ip_address',
+                'ip_latitude',
+                'ip_longitude',
+                'ip_country_code',
+                'ip_country_name',
+                'ip_region',
+                'ip_city',
+                'ip_postal_zip_code',
+                'ip_timezone',
+                'ip_currency',
+            ])
             ->whereIn('ip_address', $ipAddresses)
             ->orderBy('requested_on', 'desc')
-            ->groupBy(['ip_address', 'requested_on'])
+            ->groupBy([
+                'requested_on',
+                'ip_address',
+                'ip_latitude',
+                'ip_longitude',
+                'ip_country_code',
+                'ip_country_name',
+                'ip_region',
+                'ip_city',
+                'ip_postal_zip_code',
+                'ip_timezone',
+                'ip_currency',
+            ])
             ->get();
 
         return $matchingRequests;