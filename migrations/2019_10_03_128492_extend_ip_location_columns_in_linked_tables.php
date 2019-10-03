28a8c2edf843f4b170c37f0929b8b88e9e63cce3
calebfavor <calebfavor@gmail.com>
2019-10-03 13:17:16 -0700

    Fixing linked ip tracking data issues.

---
+++ migrations/2019_10_03_128492_extend_ip_location_columns_in_linked_tables.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ExtendIpLocationColumnsInLinkedTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $tablePrefix = config('railtracker.table_prefix') ?? 'railtracker3_';
+
+        Schema::disableForeignKeyConstraints();
+
+        Schema::table($tablePrefix . 'ip_latitudes', function (Blueprint $table) {
+            $table->decimal('ip_latitude', 11, 8)->change();
+        });
+
+        Schema::table($tablePrefix . 'ip_longitudes', function (Blueprint $table) {
+            $table->decimal('ip_longitude', 11, 8)->change();
+        });
+
+        Schema::enableForeignKeyConstraints();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $tablePrefix = config('railtracker.table_prefix') ?? 'railtracker3_';
+
+        Schema::disableForeignKeyConstraints();
+
+        Schema::table($tablePrefix . 'ip_latitudes', function (Blueprint $table) {
+            $table->decimal('ip_latitude', 10, 8)->change();
+        });
+
+        Schema::table($tablePrefix . 'ip_longitudes', function (Blueprint $table) {
+            $table->decimal('ip_longitude', 10, 8)->change();
+        });
+
+        Schema::enableForeignKeyConstraints();
+    }
+}
