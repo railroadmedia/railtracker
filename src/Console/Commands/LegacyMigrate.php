6a6f1ffc9f188d3165b4b2d4d74dd628e2c73470
Jonathan <jonathan@drumeo.com>
2020-01-21 08:24:39 -0800

    generally, wip
    specifically includes but not limited to association-table persisting fixed

--- src/Console/Commands/LegacyMigrate.php
+++ src/Console/Commands/LegacyMigrate.php
@@ -7,6 +7,8 @@ use Illuminate\Cookie\CookieJar;
 use Illuminate\Database\DatabaseManager;
 use Illuminate\Support\Collection;
 use Railroad\Railtracker\Events\RequestTracked;
+use Railroad\Railtracker\QueryBuilders\BulkInsertOrUpdateBuilder;
+use Railroad\Railtracker\QueryBuilders\BulkInsertOrUpdateMySqlGrammar;
 use Railroad\Railtracker\Repositories\RequestRepository;
 use Railroad\Railtracker\Services\BatchService;
 use Railroad\Railtracker\Services\IpDataApiSdkService;
@@ -36,18 +38,21 @@ class LegacyMigrate extends \Illuminate\Console\Command
      */
     private $requestRepository;
 
+    private $chunkSize = 100;
+    private $limit = 100;
+
     private static $map = [
-        'url_protocols' => [ // table
-            ['url_protocol' => 'url_protocol'], // row-name => property-name-in-processing-below
-            ['url_protocol' => 'referer_url_protocol'],
+        'url_protocols' => [ // 'association table' => fields required in table for each property of legacy data
+            ['url_protocol' => 'url_protocol'], // column-name => property name in JOINed data
+            ['url_protocol' => 'url_referer_protocol'],
         ],
         'url_domains' => [
             ['url_domain' => 'url_name'],
-            ['url_domain' => 'referer_url_name'],
+            ['url_domain' => 'url_referer_name'],
         ],
         'url_paths' => [
-            ['url_path' => 'urlPath'],
-            ['url_path' => 'refererUrlPath'],
+            ['url_path' => 'url_path'],
+            ['url_path' => 'url_referer_path'],
         ],
         'methods' => [
             ['method' => 'method_method'],
@@ -130,24 +135,24 @@ class LegacyMigrate extends \Illuminate\Console\Command
         'url_queries' => [ // table
             [ // a row
                 'url_query' => 'url_query_string',
-                //'url_query_hash' => 'urlQueryHash', // todo: generate hash or get from legacy?
+                'url_query_hash' => 'url_query_string_hash', // these are manually created in method "" below.
             ],
             [ // another row
-                'url_query' => 'url_query_string',
-                //'url_query_hash' => 'refererUrlQueryHash',  // todo: generate hash or get from legacy?
+                'url_query' => 'url_referer_query_string',
+                'url_query_hash' => 'url_referer_query_string_hash', // these are manually created in method "" below.
             ],
         ],
 
         'route_actions' => [
             [
                 'route_action' => 'route_action',
-                //'route_action_hash' => 'routeActionHash', // todo: generate hash or get from legacy?
+                'route_action_hash' => 'route_action_hash', // these are manually created in method "" below.
             ],
         ],
         'agent_strings' => [
             [
                 'agent_string' => 'agent_name',
-                //'agent_string_hash' => 'agentStringHash',  // todo: generate hash or get from legacy?
+                'agent_string_hash' => 'agent_name_hash', // these are manually created in method "" below.
             ],
         ],
 //        'exception_classes' => [
@@ -241,69 +246,7 @@ class LegacyMigrate extends \Illuminate\Console\Command
     {
         $this->info('------ STARTING DrumeoLegacyTo4 ------');
 
-        /*
-        railtracker4_agent_browser_versions
-        railtracker4_agent_browsers
-        railtracker4_agent_strings
-        railtracker4_device_kinds
-        railtracker4_device_models
-        railtracker4_device_platforms
-        railtracker4_device_versions
-        railtracker4_exception_classes
-        railtracker4_exception_codes
-        railtracker4_exception_files
-        railtracker4_exception_lines
-        railtracker4_exception_messages
-        railtracker4_exception_traces
-        railtracker4_ip_addresses
-        railtracker4_ip_cities
-        railtracker4_ip_country_codes
-        railtracker4_ip_country_names
-        railtracker4_ip_currencies
-        railtracker4_ip_latitudes
-        railtracker4_ip_longitudes
-        railtracker4_ip_postal_zip_codes
-        railtracker4_ip_regions
-        railtracker4_ip_timezones
-        railtracker4_language_preferences
-        railtracker4_language_ranges
-        railtracker4_methods
-        railtracker4_requests
-        railtracker4_response_durations
-        railtracker4_response_status_codes
-        railtracker4_route_actions
-        railtracker4_route_names
-        railtracker4_url_domains
-        railtracker4_url_paths
-        railtracker4_url_protocols
-        railtracker4_url_queries
-         */
-
-        /*
-        railtracker_exceptions
-        railtracker_geoip
-        railtracker_media_playback_sessions
-        railtracker_media_playback_types
-        railtracker_request_agents
-        railtracker_request_devices
-        railtracker_request_exceptions
-        railtracker_request_languages
-        railtracker_request_methods
-        railtracker_requests
-        railtracker_response_status_codes
-        railtracker_responses
-        railtracker_routes
-        railtracker_url_domains
-        railtracker_url_paths
-        railtracker_url_protocols
-        railtracker_url_queries
-        railtracker_urls
-         */
-
-//        $chunkSize = 100;
-        $chunkSize = 10;
-
-        $results = $this->databaseManager
+        $success = $this->databaseManager
             ->table('railtracker_requests')
             ->select(
                 'railtracker_requests.*',
@@ -344,14 +287,14 @@ class LegacyMigrate extends \Illuminate\Console\Command
             ->addSelect(
                 'railtracker_routes.name as route_name',
                 'railtracker_routes.action as route_action',
-                'railtracker_routes.hash as route_hash',
+                'railtracker_routes.hash as route_hash', // is this needed?
 
                 'railtracker_request_devices.kind as device_kind',
                 'railtracker_request_devices.model as device_model',
                 'railtracker_request_devices.platform as device_platform',
                 'railtracker_request_devices.platform_version as device_platform_version',
                 'railtracker_request_devices.is_mobile as device_is_mobile',
-                'railtracker_request_devices.hash as device_hash',
+                'railtracker_request_devices.hash as device_hash', // is this needed?
 
                 'railtracker_request_agents.name as agent_name',
                 'railtracker_request_agents.browser as agent_browser',
@@ -372,7 +315,7 @@ class LegacyMigrate extends \Illuminate\Console\Command
                 'railtracker_geoip.ip_address as geoip_ip_address',
                 'railtracker_geoip.timezone as geoip_timezone',
                 'railtracker_geoip.currency as geoip_currency',
-                'railtracker_geoip.hash as geoip_hash'
+                'railtracker_geoip.hash as geoip_hash' // is this needed?
             )
 
             /*
@@ -383,60 +326,195 @@ class LegacyMigrate extends \Illuminate\Console\Command
              */
 
             ->orderBy('id')
-            ->chunk($chunkSize, function($rows){
+            ->limit($this->limit)
+            ->chunk($this->chunkSize, function($rows){
                 $this->migrateTheseRequests($rows);
             });
 
-        dump($results);
+        $this->info('Success: ' . var_export($success, true));
+    }
+
+    private function fillHashes(Collection &$legacyData)
+    {
+//        $legacyDataWithHashes = [];
+//
+//        $setHashUnlessNull = function($value){
+//            return !empty($value) ? md5($value) : null;
+//        };
+
+        foreach($legacyData as &$legacyDatum){
+            /* * * * * * * * * * * * * * * * * * * * * * * *
+                'url_query_hash' => 'urlQueryHash',
+                'url_query_hash' => 'refererUrlQueryHash',
+                'route_action_hash' => 'routeActionHash',
+                'agent_string_hash' => 'agentStringHash',
+             * * * * * * * * * * * * * * * * * * * * * * * */
+
+            /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+                Given all this, are these actually needed in JOIN?
+                    'railtracker_routes.hash as route_hash'
+                    'railtracker_request_devices.hash as device_hash'
+                    'railtracker_geoip.hash as geoip_hash'
+             * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+            $legacyDatum->url_query_string_hash = null;
+            $legacyDatum->url_referer_query_string_hash = null;
+            $legacyDatum->route_action_hash = null;
+            $legacyDatum->agent_name_hash = null;
+
+            //dd($legacyDatum);
+
+            if(isset($legacyDatum->url_query_string)){
+                $legacyDatum->url_query_string_hash = md5($legacyDatum->url_query_string);
+            }
+            if(isset($legacyDatum->url_referer_query_string)){
+                $legacyDatum->url_referer_query_string_hash = md5($legacyDatum->url_referer_query_string);
+            }
+            if(isset($legacyDatum->route_action)){
+                $legacyDatum->route_action_hash = md5($legacyDatum->route_action);
+            }
+            if(isset($legacyDatum->agent_name)){
+                $legacyDatum->agent_name_hash = md5($legacyDatum->agent_name);
+            }
+        }
+
+        //dd($legacyData);
     }
 
     private function migrateTheseRequests(Collection $legacyData)
     {
+        $this->fillHashes($legacyData);
 
+        $dbConnectionName = config('railtracker.database_connection_name');
 
-//        foreach($legacyData as $row){
-//
-//        }
-
-        //$this->requestRepository->storeRequests();
+        $builder = new BulkInsertOrUpdateBuilder(
+            $this->databaseManager->connection($dbConnectionName),
+            new BulkInsertOrUpdateMySqlGrammar()
+        );
 
         // first, linked data
 
-        // 1.1 - prep linked data
+        foreach(self::$map as $table => $fieldsForEachRow){
 
-        foreach(self::$map as $table => $columnAndPropertyLabelHere){
+            $rowsToCreate = [];
 
-//            dump($columnAndPropertyLabelHere);
-
-            // note: will typically be just one, but sometimes is two, so just use foreach used anyways
-            foreach($columnAndPropertyLabelHere as $columnPropertySets){
-
+            // 1.1 - prep linked data
+            foreach($fieldsForEachRow as $columnPropertySets){
                 foreach($legacyData as $legacyDatum){
-
+                    $row = [];
                     foreach($columnPropertySets as $column => $property){
                         if(!isset($legacyDatum->$property)) continue;
-                        $value =  $legacyDatum->$property;
-                        $rowsToCreateByColumn[$table][$column][] = $value;
+                        $row[$column] = $legacyDatum->$property;
                     }
+                    if(empty($row)) continue;
+                    $rowsToCreate[] = $row;
                 }
+            }
+
+            // 1.2 - store linked data (Note: copied from RequestRepository, but sqlite part omitted)
+            if(empty($rowsToCreate)) continue;
+            try{
+                $builder->from(config('railtracker.table_prefix') . $table)->insertOrUpdate($rowsToCreate);
+            }catch(\Exception $e){
+                error_log($e);
+                $this->info('Error while writing to association tables ("' . $e->getMessage() . '")');
             }
         }
 
-        dd($rowsToCreateByColumn ?? []);
-
-        die();
-
-        // 1.2 - store linked data
-
-
         // second, store requests table
 
+        $bulkInsertData = [];
 
+        // -------------------------- replace this -----------------------------
 
+//        /**
+//         * @var $requestVOs RequestVO[]
+//         */
+//        foreach ($requestVOs as $requestVO) {
+//            $bulkInsertData[] = $requestVO->returnArrayForDatabaseInteraction();
+//        }
 
+        // ---------------------------- with this ------------------------------
 
+        foreach($legacyData as $legacyDatum){
+            $bulkInsertData[] = [
+                'uuid' =>                   $legacyDatum->uuid ?? null,                         // [x]
+                'cookie_id' =>              $legacyDatum->cookie_id ?? null,                    // [x]
+                'user_id' =>                $legacyDatum->user_id ?? null,                      // [x]
+                'url_protocol' =>           $legacyDatum->url_protocol ?? null,                 // [x]
+                'url_domain' =>             $legacyDatum->url_name ?? null,                     // [x]
+                'url_path' =>               $legacyDatum->url_path ?? null,                     // [x]
+                'method' =>                 $legacyDatum->method_method ?? null,                       // [ ]
+                'route_name' =>             $legacyDatum->routeName ?? null,                    // [ ]
+                'device_kind' =>            $legacyDatum->deviceKind ?? null,                   // [ ]
+                'device_model' =>           $legacyDatum->deviceModel ?? null,                  // [ ]
+                'device_platform' =>        $legacyDatum->devicePlatform ?? null,               // [ ]
+                'device_version' =>         $legacyDatum->deviceVersion ?? null,                // [ ]
+                'device_is_mobile' =>       $legacyDatum->deviceIsMobile ?? null,               // [ ]
+                'agent_browser' =>          $legacyDatum->agentBrowser ?? null,                 // [ ]
+                'agent_browser_version' =>  $legacyDatum->agentBrowserVersion ?? null,          // [ ]
+                'referer_url_protocol' =>   $legacyDatum->refererUrlProtocol ?? null,           // [ ]
+                'referer_url_domain' =>     $legacyDatum->refererUrlDomain ?? null,             // [ ]
+                'referer_url_path' =>       $legacyDatum->refererUrlPath ?? null,               // [ ]
+                'language_preference' =>    $legacyDatum->languagePreference ?? null,           // [ ]
+                'language_range' =>         $legacyDatum->languageRange ?? null,                // [ ]
+                'ip_address' =>             $legacyDatum->ipAddress ?? null,                    // [ ]
+                'ip_latitude' =>            $legacyDatum->ipLatitude ?? null,                   // [ ]
+                'ip_longitude' =>           $legacyDatum->ipLongitude ?? null,                  // [ ]
+                'ip_country_code' =>        $legacyDatum->ipCountryCode ?? null,                // [ ]
+                'ip_country_name' =>        $legacyDatum->ipCountryName ?? null,                // [ ]
+                'ip_region' =>              $legacyDatum->ipRegion ?? null,                     // [ ]
+                'ip_city' =>                $legacyDatum->ipCity ?? null,                       // [ ]
+                'ip_postal_zip_code' =>     $legacyDatum->ipPostalZipCode ?? null,              // [ ]
+                'ip_timezone' =>            $legacyDatum->ipTimezone ?? null,                   // [ ]
+                'ip_currency' =>            $legacyDatum->ipCurrency ?? null,                   // [ ]
+                'is_robot' =>               $legacyDatum->isRobot ?? null,                      // [ ]
 
-        die(); die(); die(); die(); die(); die(); die(); die();
+                'exception_code' =>         $legacyDatum->exceptionCode ?? null,                // [ ]
+                'exception_line' =>         $legacyDatum->exceptionLine ?? null,                // [ ]
+
+                'requested_on' =>           $legacyDatum->requestedOn ?? null,                  // [ ]
+                'response_status_code' =>   $legacyDatum->responseStatusCode ?? null,           // [ ]
+                'response_duration_ms' =>   $legacyDatum->responseDurationMs ?? null,           // [ ]
+                'responded_on' =>           $legacyDatum->respondedOn ?? null,                  // [ ]
+
+                'url_query_hash' =>         $legacyDatum->url_query_string_hash ?? null,        // [x]
+                'referer_url_query_hash' => $legacyDatum->url_referer_query_string_hash ?? null,// [x]
+                'route_action_hash' =>      $legacyDatum->routeActionHash ?? null,              // [ ]
+                'agent_string_hash' =>      $legacyDatum->agentStringHash ?? null,              // [ ]
+                'exception_class_hash' =>   $legacyDatum->exceptionClassHash ?? null,           // [ ]
+                'exception_file_hash' =>    $legacyDatum->exceptionFileHash ?? null,            // [ ]
+                'exception_message_hash' => $legacyDatum->exceptionMessageHash ?? null,         // [ ]
+                'exception_trace_hash' =>   $legacyDatum->exceptionTraceHash ?? null,           // [ ]
+            ];
+        }
+
+        // ---------------------------------------------------------------------
+
+        $table = config('railtracker.table_prefix') . 'requests';
+
+        foreach(array_chunk($bulkInsertData, $this->chunkSize) as $chunkOfBulkInsertData){
+
+            if (empty($chunkOfBulkInsertData)) continue;
+
+            try{
+                $builder->from($table)->insertOrUpdate($chunkOfBulkInsertData);
+            }catch(\Exception $e){
+                error_log($e);
+                dump('Error while writing to requests table ("' . $e->getMessage() . '")');
+            }
+        }
+
+        $uuids = array_column($chunkOfBulkInsertData ?? [], 'uuid');
+
+        // because we cant' get created rows from insert, it seems
+        $presumablyCreatedRows = $builder
+            ->from($table)
+            ->select()
+            ->whereIn('uuid', $uuids)
+            ->get();
+
+        return $presumablyCreatedRows ?? new Collection();
     }
 
     private function Drumeo3To4()
