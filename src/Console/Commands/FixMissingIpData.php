8d92ad40f50919828a324f3896f90e0a49ea8fed
Jonathan <jonathan@drumeo.com>
2020-03-14 19:23:55 -0700

    wip

--- src/Console/Commands/FixMissingIpData.php
+++ src/Console/Commands/FixMissingIpData.php
@@ -3,6 +3,7 @@
 namespace Railroad\Railtracker\Console\Commands;
 
 use Illuminate\Database\DatabaseManager;
+use Illuminate\Support\Collection;
 use Railroad\Railtracker\Services\IpDataApiSdkService;
 
 class FixMissingIpData extends \Illuminate\Console\Command
@@ -72,27 +73,76 @@ class FixMissingIpData extends \Illuminate\Console\Command
          */
         $this->info('(csvForTable),chunk size,request table updates,association tables updates');
 
-        $this->databaseManager
-            ->table($table)
-            ->select('id', 'ip_address')
-            ->where(['ip_longitude' => null, 'ip_latitude' => null])
-            ->groupBy('ip_address', 'id')
-            ->orderBy('id')
-            ->chunkById(100, function($ip_addresses) use ($updates){
-                $updateStatusInfo = $this->fillForIpAddresses($ip_addresses);
-                $this->info(
-                    '(csvForTable),' .
-                    count($ip_addresses) . ',' .
-                    $updateStatusInfo['requestTableUpdatesCount'] . ',' .
-                    $updateStatusInfo['associationTableUpdateCount']
-                );
-            });
+
+        /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+        This old version is SHIT - it fucking shreks the prod server. Absolute bullshit. Kill it with fire!
+        ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+
+        The problem is that it has to query a massive amount of data. with a very fucking broad filter. It gets better
+        if we also filter by `ip_address IS NOT NULL`
+
+        * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+//        $this->databaseManager
+//            ->table($table)
+//            ->select('id', 'ip_address')
+//            ->where(['ip_longitude' => null, 'ip_latitude' => null])
+//            ->groupBy('ip_address', 'id')
+//            ->orderBy('id')
+//            ->chunkById(100, function($ip_addresses) use ($updates){
+//                $updateStatusInfo = $this->fillForIpAddresses($ip_addresses);
+//                $this->info(
+//                    '(csvForTable),' .
+//                    count($ip_addresses) . ',' .
+//                    $updateStatusInfo['requestTableUpdatesCount'] . ',' .
+//                    $updateStatusInfo['associationTableUpdateCount']
+//                );
+//            });
+
+
+        /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+        This is the new version below
+        ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+
+        To get even better than filtering by `ip_address IS NOT NULL`, **ALSO** filter by id. We *will* have a somewhat
+        convoluted loop of going through ids, but oh well, it's better than fucking destroying the DB server for like a
+        fucking hour.
+
+        * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+        // consider getting count of table (railtracker4_requests)
+
+        $idFilterMarker = 0;
+
+        // while loop
+
+            // while rows are returned keep going. when we get an empty db result then stop...? NO!! we're filtering
+            // ... here so we may sometimes not get results back. Sooooo... we need the max id, and if our
+            // "$idFilterMarker" (what we'll to set our min and max values for each query.)
+
+            // when you get results, then process that.
+
+
+
+
+
 
         return true;
     }
 
-    private function fillForIpAddresses($rowsRequiringData)
+    private function fillForIpAddresses(Collection $rowsRequiringData)
     {
+
+        $this->info('processing chunk containing these rows:');
+        $this->info('    id,ip_address');
+
+        foreach($rowsRequiringData as $row){
+            $this->info('    ' . $row->id . ',' . $row->ip_address);
+        }
+
+        $this->info('');
+
         $idsByIpAddress = [];
         $associationTableUpdateCount = 0;
 
@@ -105,6 +155,8 @@ class FixMissingIpData extends \Illuminate\Console\Command
         // Uncomment ↓ to see "total number of requests made by your API key in the last 24 hrs. Updates once a minute."
         //$this->info('API requests in past 24h: ' . end($ipData)['count'] ?? null);
 
+        dd($ipData);
+
         foreach($idsByIpAddress as $ipAddress => $rowIds){
 
             $updates = [];
@@ -112,12 +164,17 @@ class FixMissingIpData extends \Illuminate\Console\Command
             // find relevant ip data
             $relevantIpData = null;
             foreach($ipData as $candidate){
+
+                if(empty($candidate['ip'])) continue;
+
                 if($candidate['ip'] === $ipAddress){
                     $relevantIpData = $candidate;
                 };
             }
 
             if(empty($relevantIpData)) continue;
+
+            dd($relevantIpData);
 
             // prepare ip data for db insertion
             $dataForUpdate = [
