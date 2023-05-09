991ffed97f0763c518d74954880a3072405120a1
Roxana Riza <roxana_r2000@yahoo.com>
2023-05-09 18:31:52 +0300

    Delete last engaged content

--- src/Controllers/ContentLastEngagedJsonController.php
+++ src/Controllers/ContentLastEngagedJsonController.php
@@ -60,4 +60,40 @@ class ContentLastEngagedJsonController extends Controller
             201
         );
     }
+
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function delete(Request $request)
+    {
+        try {
+            $this->validate($request, [
+                'content_id' => 'required|numeric',
+                'parent_playlist_id' => 'integer',
+                'parent_content_id' => 'integer',
+            ]);
+        } catch (ValidationException $exception) {
+            throw new HttpResponseException(
+                response()->json(
+                    [
+                        'errors' => $exception->errors(),
+                    ],
+                    422
+                )
+            );
+        }
+
+        $this->contentLastEngagedService->deleteEngagedContent(
+            auth()->id(),
+            $request->input('content_id'),
+            $request->input('parent_playlist_id'),
+            $request->input('parent_content_id')
+        );
+
+        return response()->json(
+            null,
+            204
+        );
+    }
 }
